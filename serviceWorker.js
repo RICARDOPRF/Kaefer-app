7ef229987f3b1d0c6c2d7f015b98a136f463fc9e
RICARDOPRF <paulo.freitas@kaefer.com>
2025-08-01 01:23:36 -0300

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,18 +1,24 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('kaefer-cache').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html'
-      ]);
-    })
+const CACHE_NAME = "kaefer-app-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./style.css",
+  "./script.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
